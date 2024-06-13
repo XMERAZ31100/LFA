07969028bf994d9401a6e6a36c73d01016957269
XMERAZ31100 <158120737+XMERAZ31100@users.noreply.github.com>
2024-06-13 00:41:40 -0600

    Add files via upload

---
+++ xmc/UNIDAD_1_/Cerradura_suma/Suma1.3.py
@@ -0,0 +1,13 @@
+#PEDIRLE AL USUARIO QUE INGRESE DOS NUMEROS FLOTANTES
+print("INGRESE DOS NUMEROS FLOTANTES")
+num1 = float(input("ingrese el primer numero: "))
+num2 = float(input("ingrese el segundo numero: "))
+
+#COMPARAR LOS NUMEROS
+if  num1 > num2 :
+    print("el primer numero es mayor que el segundo numero ")
+elif num1<num2:
+    print("el segundo numero es mayor que el primer numero ")
+    format((num1,num2))
+else:
+    print("los numeros son iguales")
